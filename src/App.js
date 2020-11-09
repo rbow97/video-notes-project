62e7433744fd74e5a6557dd48af3994390de50db
Rose Bowen <rosebowen1997@gmail.com>
2020-11-09 21:50:06 +0000

    trying to get url from input value

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const { inputValue, setInputValue } = useState("");
+
+  if (inputValue) {
+    console.log("test");
+  }
+  console.log("rose");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>
+          URL:
+          <input type="text" name="name" />
+        </label>
+        <input
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          type="submit"
+          value={inputValue}
+        />
+      </form>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/h0U2QUGKbSE"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </div>
   );
-}
+};
 
 export default App;
