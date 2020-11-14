7110e728d1ddac13e7266aa89bbe4ff6905d1d37
Joe Smith <jmjsmith23@gmail.com>
2020-11-14 13:33:58 +0000

    Cleanup of stuff we don't need - new testing file created - css cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useState, useEffect } from "react";
 
@@ -25,15 +24,16 @@ const App = () => {
           value={inputValue}
         />
       </form>
-      <div id="player"></div>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/h0U2QUGKbSE"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+      <div id="player">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/h0U2QUGKbSE"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   );
 };
